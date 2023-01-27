4774e5726e389b288c9d9f14692bfb2dbc041161
paperClub-hub <qhs2018@sina.com>
2023-01-27 13:52:24 +0800

    init

---
+++ f20230105_lask_demo/app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @ Date    : 2023/1/5 13:55
+# @ Author  : paperClub
+# @ Email   : paperclub@163.com
+# @ Site    :
+
+
+from flask import Flask, request, render_template
+
+app = Flask(__name__, template_folder='./templates')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    # return render_template('home.html')
+    return render_template('form.html')
+
+
+@app.route('/signin', methods=['GET'])
+def signin_form():
+    return render_template('form.html')
+
+
+@app.route('/signin', methods=['POST'])
+def signin():
+    username = request.form['username']
+    password = request.form['password']
+    if username=='admin' and password=='password':
+        return render_template('signin-ok.html', username=username)
+    return render_template('form.html', message='Bad username or password', username=username)
+
+
+if __name__ == '__main__':
+    app.run()
