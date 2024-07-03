ea27e5425e131b21d52610156f5f3ef0b187547c
paperClub-hub <paperclub@163.com>
2024-07-03 11:30:58 +0800

    添加超时终止方法

--- 20240702_超时处理/test.py
+++ 20240702_超时处理/test.py
@@ -5,12 +5,12 @@
 # @Author  : Administrator
 # @Desc    :
 
-
 import os
 import time
 import threading
 import subprocess
 from typing import Callable
+from datetime import datetime
 from multiprocessing import Process, Queue
 
 
@@ -71,10 +71,10 @@ def run_task2(function:Callable, args, timeout):
 
 
 # 对上面方法进行改进
-from multiprocessing import Process, Queue
-from datetime import datetime
+
 
 def get_wrapper(q, urls):
+    from langchain_community.document_loaders import AsyncHtmlLoader
     loader = AsyncHtmlLoader(urls)
     docs = loader.load()
     for doc in docs:
@@ -90,7 +90,7 @@ def run_with_timeout(func, timeout, *args):
     p.start()
     p.join(timeout) # 等待进程完成或超时
     if p.is_alive():
-        print(f"{datetime.now()} {str(func)}执行已超时({timeout}s)，正在终止进程...")
+        print(f"{datetime.now()} {str(func)} 执行已超时 ({timeout}s)， 正在终止进程...")
         p.terminate()  # 超时终止进程，
         p.join()  # 确保进程已经终止，超时情况下，我们没有结果
         result = []
@@ -98,15 +98,6 @@ def run_with_timeout(func, timeout, *args):
         print(f"{datetime.now()} {str(func)}执行成功完成")
         result = q.get()  # 从队列中获取结果
     return result
-
-
-t1 = time.time()
-timeout = 10
-urls = ['https://baike.baidu.com/item/韦小宝/1448539', 'https://zh.wikipedia.org/zh-sg/韋小寶', 'https://www.sohu.com/a/215419865_648199']
-docs = run_with_timeout(get_wrapper, timeout, urls)
-t2 = time.time()
-print("耗时：", t2 - t1)
-print("dccs: ", docs)
 
 
 # --------------------- 基于subprocess, 适用调用外部方法
@@ -136,6 +127,15 @@ if __name__ == '__main__':
     duration = 4  # 子函数运行超过3秒则提示超时
     res = run_task2(sub_function, (duration,), timeout)
     print("res: ", type(res), res)
+
+    t1 = time.time()
+    timeout = 10
+    urls = ['https://baike.baidu.com/item/韦小宝/1448539', 'https://zh.wikipedia.org/zh-sg/韋小寶',
+            'https://www.sohu.com/a/215419865_648199']
+    docs = run_with_timeout(get_wrapper, timeout, urls)
+    t2 = time.time()
+    print("耗时：", t2 - t1)
+    print("dccs: ", docs)
 
     # # 示例调用
     # cmd = ['python', 'xxx.py']
